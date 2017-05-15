9770444be00ed92ddbc85013cfe612e2335af181
Karoline Brehm <karoline.engel.brehm@uni-weimar.de>
2017-05-15 17:26:11 +0200

    Aufgaben 3.3,3.4 fertig \(^-^)/

--- source/aufgabe2bis4.cpp
+++ source/aufgabe2bis4.cpp
@@ -5,9 +5,17 @@
 #include <iterator> // std::ostream_iterator<>
 #include <algorithm> // std::reverse, std::generate
 #include <string>
+#include <set>
+#include <map>
 
 int main ()
 {
+
+	/*
+	
+	Aufg. 3.2
+
+	*/
 
 	//list mit unsigned int initialisieren
 	std::list<unsigned int> liste(100);
@@ -28,6 +36,7 @@ int main ()
 	*/
 
 	//Ausgabe liste
+	std::cout << " \n liste: \n";
 	std::copy(std::begin(liste), std::end(liste),
 				std::ostream_iterator<int>(std::cout, " "));
 
@@ -40,4 +49,58 @@ int main ()
 				std::ostream_iterator<int>(std::cout, " "));
 	std::cout << "\n";
 
+	/*
+	
+	Aufg. 3.3
+
+	*/
+	
+	std::set<int> zahlen (liste.begin(), liste.end());
+	//Ausgabe Anzahl unterschiedliche Zahlen
+	std::cout << "\nAnzahl unterschiedliche Zahlen: "
+		<< zahlen.size() <<"\n";
+
+	std::cout << "Nicht enthaltene Zahlen: \n";
+	for (int i = 0; i<=100; ++i)
+	{
+		if (zahlen.find(i) == zahlen.end())
+		{
+			std::cout << i << " ";
+		}
+	}
+
+	/*
+	
+	Aufg. 3.4
+	Map ist geeignet, da hier Key+Value gespeichert werden
+	Zahlen in Set sind keys, und eindeutig, da für jede zahl
+	ihre Häufigkeit angegeben werden soll, welche man als
+	entsprechenden value speichern kann
+	*/	
+
+	//map erstellen
+	std::map<unsigned int, int> haufigkeiten;
+
+	//Paare aus Zahl / Haufigkeit zu Map hinzufügen
+	for (auto& key : zahlen)
+	{	
+		int counter = 0;
+		for (auto& value : liste)
+		{
+			if(key == value)
+			{
+				++counter;
+			}
+		}
+		haufigkeiten[key] = counter;
+	}
+
+	std::cout << "\nHaufigkeiten: \n";
+
+	//map ausgeben in form key : value
+	for (auto& i : haufigkeiten)
+	{
+		//i ist pair (first, second) mit (key, value)
+		std::cout << i.first << " : " << i.second << "\n";
+	}
 }
